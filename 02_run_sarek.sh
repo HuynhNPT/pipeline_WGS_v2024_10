486332ec343be5f570051e058b9aa0d52a9c68d3
HuynhNPT <gwennhuynh@gmail.com>
2024-11-04 15:21:48 +0000

    Modify 02_run_sarek to have a PROJECT placeholder

--- 02_run_sarek.sh
+++ 02_run_sarek.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PROJECT=""
+
 nextflow run nf-core/sarek \
             -r 3.4.4 \
             -with-report \
@@ -9,5 +11,5 @@ nextflow run nf-core/sarek \
             --igenomes_base  gs://nextflow-batch-input/_resources/genomeMapping/GATK \
             --genome GATK.GRCh38 \
             --trim_fastq 0 \
-            --outdir gs://nextflow-batch-output/WGS/Broad_240829_noTRIM \
+            --outdir gs://nextflow-batch-output/WGS/${PROJECT} \
             --tools cnvkit,deepvariant,haplotypecaller,manta,strelka
