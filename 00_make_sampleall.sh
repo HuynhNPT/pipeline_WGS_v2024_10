0ca77c0021f6429e8b26804bbeba4ace168dd80b
HuynhNPT <gwennhuynh@gmail.com>
2025-05-19 15:40:48 +0000

    Change 00_make_sampleall to get multiple buckets

--- 00_make_sampleall.sh
+++ 00_make_sampleall.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-export projectDir="gs://nextflow-batch-input/platform/WGS/Broad_240829/download_broad_8_29_2024/"
+export projectDir="gs://nextflow-batch-input/platform/WGS/250425_Broad/download_broad_4_25_2025/"
 
-gsutil ls "${projectDir}"*cram | sed 's#.*/##' | sed 's/\.cram//' > sample_list
-cat sample_list | sed 's/$/\.cram/' > cram
-cat sample_list | sed 's/$/\.cram\.crai/' > crai
-cat sample_list | sed 's/$/\.cram\.md5sum/'  > md5
+gsutil ls "${projectDir}"*cram | sed 's#.*/##' | sed 's/\.cram//' > sample_list1
+cat sample_list1 | sed 's/$/\.cram/' | sed 's#^#250425_Broad/download_broad_4_25_2025/#' > cram
+cat sample_list1 | sed 's/$/\.cram\.crai/' | sed 's#^#250425_Broad/download_broad_4_25_2025/#' > crai
+cat sample_list1 | sed 's/$/\.cram\.md5sum/'  | sed 's#^#250425_Broad/download_broad_4_25_2025/#' > md5
+
+export projectDir="gs://nextflow-batch-input/platform/WGS/250430_Broad/download_broad_4_30_2025/"
+
+gsutil ls "${projectDir}"*cram | sed 's#.*/##' | sed 's/\.cram//' > sample_list2
+cat sample_list2 | sed 's/$/\.cram/' | sed 's#^#250430_Broad/download_broad_4_30_2025/#' >> cram
+cat sample_list2 | sed 's/$/\.cram\.crai/' | sed 's#^#250430_Broad/download_broad_4_30_2025/#' >> crai
+cat sample_list2 | sed 's/$/\.cram\.md5sum/'  | sed 's#^#250430_Broad/download_broad_4_30_2025/#' >> md5
+
+cat sample_list1 sample_list2 > sample_list
 
 wc -l cram; wc -l crai; wc -l md5; wc -l sample_list
 paste -d ',' sample_list cram crai md5 > samples_all
